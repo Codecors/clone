f11d8439af47edb88e0c1998a678364ab4d398a7
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-06-10 08:22:46 +0100

    first draft of tool

---
+++ gew.js
@@ -0,0 +1,385 @@
+/*
+ * Code to handle user clicks on SVG emotion wheel embedded in html page.
+ * User can select up to a certain number (MAX_CHOICES) of emotions,
+ * only one of each label
+ *
+ * number of emotions, number of strengths, circle ids are all hard-coded.
+ *
+ * Andy Brown.
+ * andy.brown01@bbc.co.uk
+ * BBC R&D. May 2015
+ *
+ */
+
+/* check that the svg wheel is loaded - if so, add listeners, else, wait and try again */
+function checkReady() {
+    var svg = document.getElementById("wheel-svg").getSVGDocument();
+    if (svg == null) {
+        setTimeout("checkReady()", 300);
+    } else {
+        addFeedbackFields();
+		addCircleListeners();
+    }
+}
+
+
+var svgDoc;
+checkReady();
+
+
+// wheel contains groups of 4 circles, one group per emotion
+// ids are circleN-X
+// N is number 1-4 representing strength of emotion
+// X is letter to differentiate emotions
+
+// the letters
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+	'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'];
+
+// how many are they allowed to select?
+var MAX_CHOICES = 3;
+
+// the labels, as map to letters above
+var labels = ['shock', 'interest', 'anger', 'hate', 'amusement',
+	'pride', 'joy', 'contentment', 'contempt', 'disgust', 'fear',
+	'confusion', 'disappointment', 'relief', 'admiration', 'love',
+	'guilt', 'regret', 'compassion', 'sadness'];
+var otherLabel = null;
+
+// a list of which circle ids are currently selected
+var selection = [];
+// a list of which circle ids have been selected since last submit
+var allSelected = [];
+
+
+/* add some lines to show which emotions have been selected */
+function addFeedbackFields(){
+    // First set intructions to reflect number of choices
+    var instruct1 = document.getElementById("number");
+    instruct1.innerHTML = MAX_CHOICES;
+    var instruct2 = document.getElementById("number2");
+    instruct2.innerHTML = MAX_CHOICES;
+
+    // second, add feedback area fields
+    var feedbackDiv = document.getElementById("feedback");
+    for (var i = 0; i < MAX_CHOICES; i++){
+            var fbEmo = document.createElement("div");
+            var lab = document.createTextNode("Emotion " + (i+1) + ": ");
+            fbEmo.appendChild(lab);
+            var display = document.createElement("input");
+            display.name = 'emo' + (i+1);
+            display.id = 'emotion' + (i+1);
+            display.type = 'text';
+            fbEmo.appendChild(display);
+            var colBlk = document.createElement("div");
+            colBlk.id = 'emo' + (i+1);
+            colBlk.className = 'indicator';
+            fbEmo.appendChild(colBlk);
+            feedbackDiv.appendChild(fbEmo);
+    }
+
+}
+
+/* add a listener to each circle in the wheel, plus neutral and other */
+function addCircleListeners(){
+	var svg = document.getElementById("wheel-svg");
+	svgDoc = svg.contentDocument;
+
+	// main emotions
+	for(var i = 0; i < letters.length; i++){
+		for(var j = 1; j <= 4; j++){
+			var circle_id = "circle" + j + "-" + letters[i];
+			enable(circle_id);
+		}
+	}
+
+	// neutral and other
+	enable('circle-neutral');
+	enable('circle-other');
+}
+
+
+/* this circle has been clicked */
+function select(circleid){
+    allSelected.push(circleid);
+
+	// update selection list
+	if (selection.length < MAX_CHOICES){
+		selection.push(circleid);
+	}
+
+    // prevent further selection if full, or neutral selected
+    if(selection.length == MAX_CHOICES || selection[0] === 'circle-neutral'){
+        stopAll();
+    }
+
+	// feedback change
+	var c = svgDoc.getElementById(circleid);
+	c.oldFill = c.style.fill;  // store original color
+	c.style.fill = 'rgb(200, 200, 200)';  // set as grey
+	c.onclick = function(){ deselect(this.id); };  // change click action
+
+	// make other circles of same emotion un-selectable
+	var etype = circleid.substring(circleid.length-1);
+	for(var i = 0; i < 4; i++){
+		var cid = 'circle' + (i+1) + "-" + etype;
+		if(cid !== circleid){
+			disable(cid);
+		}
+	}
+    // only allowed neutral on it's own
+    if(circleid !== 'circle-neutral'){
+        disable('circle-neutral');
+    }
+
+	// set form fields
+	setFields();
+
+    // get what other actually is
+    if(circleid === 'circle-other'){
+        for (var i = 0; i < MAX_CHOICES; i++){
+            if(selection[i] === 'circle-other'){
+                otherLabel = prompt('Please specify the emotion','name strength');
+                var textField = document.getElementById('emotion' + (i+1));
+                textField.value = otherLabel;
+            }
+        }
+    }
+
+}
+
+
+/* a circle has been de-selected */
+function deselect(circleid){
+    allSelected.push("-" + circleid);
+
+	// change color
+	resetColor(circleid);
+
+	// reset array
+	var ind = selection.indexOf(circleid);
+	var newSelection = [];
+	for(var i = 0; i < selection.length; i++){
+		if(i != ind){
+			newSelection.push(selection[i]);
+		}
+	}
+	selection = newSelection;
+	if(selection.length < MAX_CHOICES){
+		// re-enable selection of all except selected?
+		allowAll();
+	}
+
+	// re-enable interaction with other strengths of same emotion
+	var svg = document.getElementById("wheel-svg");
+	var svgDoc = svg.contentDocument;
+	var etype = circleid.substring(circleid.length-1);
+	for(var i = 0; i < 4; i++){
+		var cid = 'circle' + (i+1) + "-" + etype;
+		if(cid !== circleid){
+			enable(cid);
+		}
+	}
+
+	// update form fields
+	setFields();
+}
+
+
+/* returns true if this circle is selected */
+function isSelected(circleid){
+	for(var i = 0; i < selection.length; i++){
+		if(selection[i] === circleid){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+/* returns true if this or another strength of this emotion is selected */
+function hasSiblingSelected(circleid){
+	if(circleid === 'circle-neutral' || circleid === 'circle-other'){
+		return isSelected(circleid);
+	}
+	var letter = getLetter(circleid);
+	for(var i = 0; i < selection.length; i++){
+		if(getLetter(selection[i]) === letter){
+			return true;
+		}
+	}
+	return false;
+}
+
+
+/* disallow selection of any except selected */
+function stopAll(){
+	for(var i = 0; i < letters.length; i++){
+		for(var j = 1; j <= 4; j++){
+			var circle_id = "circle" + j + "-" + letters[i];
+			if(!isSelected(circle_id)){
+				disable(circle_id);
+			}
+		}
+	}
+	if(!isSelected('circle-neutral')){
+		disable('circle-neutral');
+	}
+	if(!isSelected('circle-other')){
+		disable('circle-other');
+	}
+}
+
+
+/* allow selection of any circle, except those who have a sibling selected*/
+function allowAll(){
+	for(var i = 0; i < letters.length; i++){
+		for(var j = 1; j <= 4; j++){
+			var circle_id = "circle" + j + "-" + letters[i];
+			if(!isSelected(circle_id) &! hasSiblingSelected(circle_id)){
+				enable(circle_id);
+			}
+		}
+	}
+	if(!isSelected('circle-neutral')){
+        if(selection.length == 0){
+    		enable('circle-neutral');
+        }
+	}
+	if(!isSelected('circle-other')){
+		enable('circle-other');
+	}
+}
+
+
+/* prevent interaction with this circle */
+function disable(circleid){
+	var c = svgDoc.getElementById(circleid);
+	c.onclick = null;
+	c.style.cursor = 'auto';
+}
+
+
+/* enable interaction with this circle (normal select) */
+function enable(circleid){
+	var c = svgDoc.getElementById(circleid);
+	c.onclick = function(){ select(this.id); };
+	c.style.cursor = 'pointer';
+}
+
+
+/* get a label for given id */
+function getLabel(circleid){
+	// form is: 'label N' where label is emotion name, N is strength number
+	var emot = circleid.substring(6);
+	if (emot == '-neutral'){
+        var emo = emot.substring(1);
+    }
+    else if(emot == '-other'){
+        var c = svgDoc.getElementById(circleid);
+        if (otherLabel){
+            var emo = otherLabel;
+        }
+        else{
+    		var emo = emot.substring(1);
+        }
+	}
+	else{
+		var letter = getLetter(circleid);
+		var label = labels[letters.indexOf(letter)];
+		var strength = getNumber(circleid);
+		var emo = label + " " + strength;
+	}
+	return emo;
+}
+
+
+/* get the color a circle is by default */
+function getColor(circleid){
+	var svg = document.getElementById("wheel-svg");
+	var svgDoc = svg.contentDocument;
+	var c = svgDoc.getElementById(circleid);
+	if (c.oldFill){
+		return c.oldFill;
+	}
+	var col = c.style.fill;
+	if (col == 'rgb(255, 255, 255)'){
+		col = 'rgb(0, 0, 0)';
+	}
+	return col;
+}
+
+
+/* get the letter from this circleid (type of emotion) */
+function getLetter(circleid){
+	var emot = circleid.substring(circleid.indexOf('-'));
+	var letter = emot.substring(1);
+	return letter;
+}
+
+
+/* get the number from this circleid (strength of emotion) */
+function getNumber(circleid){
+	var emot = circleid.substring(6);
+	var strength = emot[0];
+	return strength;
+}
+
+
+/* set feedback form fields to reflect selection array */
+function setFields(){
+    // now reset
+    for (var i = 0; i < MAX_CHOICES; i++){
+        var textField = document.getElementById('emotion' + (i+1));
+        var colorBlock = document.getElementById('emo' + (i+1));
+        if(selection[i]){
+            var emo = getLabel(selection[i]);
+            var strength = getNumber(selection[i]);
+            textField.value = emo;
+            colorBlock.style.background = getColor(selection[i]);
+            colorBlock.style.opacity = strength/4;
+        }
+        else{
+            textField.value = "";
+            colorBlock.style.background = 'rgb(255, 255, 255)';
+        }
+    }
+}
+
+
+/* submit pressed - save selection via xmlhttprequest */
+function store(){
+    // generate string
+    var resultsString = new Date();
+    for (var i = 0; i < MAX_CHOICES; i++){
+        var textField = document.getElementById('emotion' + (i+1));
+        resultsString += "," + textField.value;
+    }
+
+    // send data
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST","./store.php",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send("result="+resultsString + "&selection="+selection.toString() + "&all="+allSelected.toString());
+
+    // clear wheel
+	clear();
+}
+
+
+/* set a circle back to its original color */
+function resetColor(circleid){
+	var c = svgDoc.getElementById(circleid);
+	c.style.fill = c.oldFill;
+	enable(circleid);
+}
+
+
+/* return wheel and selection to original state */
+function clear(){
+	// deselect in reverse order
+	for(var i = selection.length - 1; i >= 0; i--){
+		deselect(selection[i]);
+	}
+    allSelected = [];
+}
---
+++ store.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Handles post request with two fields, and saves to a file.
+ * Goes with emotion wheel stuff
+ *
+ * Andy Brown.
+ * andy.brown01@bbc.co.uk
+ * BBC R&D. May 2015
+ *
+ */
+
+$data = $_POST['result'].": ";
+$raw = $_POST['selection']."\n";
+$taps = $_POST['all']."\n";
+/*
+$date = $_POST['date'];
+$e1 = $_POST['emotion1'];
+$e1 = $_POST['emotion2'];
+$e1 = $_POST['emotion3'];
+$store_string = $date.": ".$e1.";".$e2.";".$e3."\n"
+*/
+file_put_contents("./emotions.txt", $data.$raw."; ".$taps, FILE_APPEND);
+?>

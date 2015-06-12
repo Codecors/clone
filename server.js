4a997b7d772e13ba1323af1bbb654518ee90ea6b
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-06-12 15:04:33 +0100

    enabled guid tracking, and modified routing to cope with get parameters

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ var fs = require('fs')
 var path = require('path');
 var url = require("url");
 
+var users = {};
+
 var playingClip = false; // are we playing a proper clip?
 
 app.listen(8002, "0.0.0.0");
@@ -24,20 +26,23 @@ var logFile = "./bbc.log";  // path is relative to where the command
 
 
 function handler (req, response) {
-        // console.log(req.url);
         _url = url.parse(req.url, true);
+        // console.log(_url);
         // console.log(url.parse(req.url, true));
         var loadFile =  '';
 		var pathname = __dirname + _url.pathname;
 		var extname = path.extname(pathname);
-        if(req.url == '/dial'){
+        if(_url.pathname == '/dial'){
                 loadFile = '/dial.html';
         }
-        else if(req.url == '/wheel'){
+        else if(_url.pathname === '/wheel'){
                 loadFile = '/emotion.html';
         }
-        else if(req.url == '/remote' || req.url == '/control'){
+        else if(_url.pathname == '/remote' || _url.pathname == '/control'){
                 loadFile = '/control.html';
+        }
+        else if(_url.pathname === '/start'){
+                loadFile = '/start.html';
         }
         else{
                 loadFile = _url.pathname;
@@ -49,7 +54,7 @@ function handler (req, response) {
     if (err) {
             console.log(err);
       response.writeHead(500);
-      return response.end('Error loading index.html');
+      return response.end('Error loading ' + req.url);
     }
 
     response.writeHead(200);//, {'Content-Type': contentType});
@@ -63,18 +68,54 @@ io.sockets.on('connection', function (socket) {
     // move to static
     // pass on requests to all clients
     socket.on('static', function (data) {
+            // if there is a pid, find out guid:
+            if(data.pid){
+                var gid = null;
+                for (var key in users) {
+                   if (users.hasOwnProperty(key)) {
+                       var obj = users[key];
+                       if(obj === data.pid){
+                           gid = key;
+                       }
+                    }
+                }
+                console.log(data.pid + " is " + gid );
+                data.guid = gid;
+            }
+
+            // broadcast change - only those with correct guid will respond
             console.log("changing page to" + data.url);
             log("loading static page index: " + data.url);
             socket.broadcast.emit('static', data);
         });
 
+    // guid wanted
+    socket.on('guid', function (data) {
+        var guid = generateGuid();
+        socket.emit('guid', { "value": guid });
+    });
+
+    // start
+    socket.on('start', function (data) {
+        console.log(data.pid + " is " + data.guid);
+        var user = data.guid;
+        users[user] = data.pid;
+        socket.emit('static', data);
+        console.log(users);
+    });
+
+
     // received data - store
     socket.on('dial', function (data) {
-            log("dial data: " + data.time + " - " + data.value);
+            var user = users[data.guid];
+            log(user + " dial data: " + data.time + " - " + data.value);
         });
 
     socket.on('wheel', function (data) {
-            log("wheel data: " + data.time + " - " + data.result);
+        var user = users[data.guid];
+            log(user + " wheel data: " + data.time + " - " + data.result);
+            // move back to dial
+            socket.emit('static', { "url": "/dial", "guid": data.guid });
         });
 
 
@@ -98,3 +139,14 @@ io.sockets.on('connection', function (socket) {
         */
         }
     });
+
+// guid generator
+function generateGuid() {
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+}

dfc7a8eecd2ba4b43c39e507150321b522ee9869
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-06-24 10:04:10 +0100

    sessions and guids now replaced by socket rooms and ids; start page now dynamically updates to display any content

--- nodeInit.js
+++ nodeInit.js
@@ -1,43 +1,52 @@
-// get a GET parameter
-function get(name){
-    if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
-       return decodeURIComponent(name[1]);
+/*
+  initiation stuff - start a socket.io session and get a participant id
+
+  also contains code to update page content dynamically
+*/
+
+// load a javascript file dynamically
+function loadJSFile(filename){
+    var fileref=document.createElement('script')
+    fileref.setAttribute("type","text/javascript")
+    fileref.setAttribute("src", filename)
+    document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
-var guid = get('uid');
-var pid = get('pid');
-// var session;
-
-if(pid){
-    document.title += " " + pid;
-}
-
+var pid = null;
+var stopLogging; // so we can stop continuous logging operations
 
 // nodejs initiation
-// will run without, but values only output to console
-var nodejs = false;
-var session;
-try{
-    var urlarr = window.location.href.split("/");
-    var server = urlarr[0] + "//" + urlarr[2]
-    var socket = io.connect(server);
-    console.log('connected');
-    nodejs = true;
+var urlarr = window.location.href.split("/");
+var server = urlarr[0] + "//" + urlarr[2]
+var socket = io.connect(server);
+console.log('connected');
+var nodejs = true;
 
-    // find session:
-    socket.emit('getSession', {"guid": guid});
-    socket.on('isSession', function(data){
-        session = data.sid;
-    })
 
-    // allow this page to be changed
-    socket.on('static', function (data) {
-        if(data.guid == guid || (data.guid === "all" && data.session === session)){
-            location.assign(data.url + "?uid=" + guid + "&pid=" + data.pid);
-        }
-    });
-}
-catch(e){
-    console.log("not saving through nodejs");
-    // no storage
+// allow the content of this page to be changed
+socket.on('static', function (data) {
+    // stop any ongoing logging
+    stopLogging = true;
+
+    // update content
+    var body = document.getElementById("content");
+    body.innerHTML = data.content;
+
+    // then get js to run...
+    var scripts = data.scripts;
+    for(var i = 0; i < scripts.length; i++){
+        loadJSFile(scripts[i]);
+    }
+
+    // set title
+    document.title = data.title + pid;
+});
+
+
+// send pid and guid to server, and which page we want to go to
+function start(destination){
+    pid = document.getElementById('participantid').value;
+    document.title += " " + pid;
+    var sid = document.getElementById('sessions').value;
+    socket.emit('start', { "pid": pid, "session": sid, "url": destination });
 }

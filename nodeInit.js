a7058ced0bf9cce07fa594d8fcf06ac4e275d673
Andy Brown (Mac) <andy.brown01@bbc.co.uk>
2015-07-20 17:31:31 +0100

    added data caching and re-joining of session after connection lost; also location collection

--- nodeInit.js
+++ nodeInit.js
@@ -12,15 +12,47 @@ function loadJSFile(filename){
     document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
-var pid = null;
+var pid = null;  // participant id
+var sid = null;  // session id
+var oldsocketid = null; // the last socket id we had, so we can rejoin
 var stopLogging; // so we can stop continuous logging operations
+var connected = false;  // is the socket connection up?
+var cache = []; // to collect data when no connection
 
 // nodejs initiation
 var urlarr = window.location.href.split("/");
 var server = urlarr[0] + "//" + urlarr[2]
 var socket = io.connect(server);
-console.log('connected');
+
+socket.on('connect', function(client){
+    console.log('connected ');
+    if(oldsocketid == null){
+        oldsocketid = socket.io.engine.id;
+    }
+    connected = true;
+});
+
+socket.on('disconnect', function(){
+    console.log("no connection");
+    connected = false;
+});
+
+socket.on('reconnect', function(){
+    console.log('reconnected');
+    connected = true;
+    if(oldsocketid != null){
+        socket.emit('rejoin', { "pid": pid, "session": sid, "oldid": oldsocketid });
+        oldsocketid = socket.id;
+    }
+    while(cache.length > 0){
+        console.log('sending cached data');
+        var cacheItem = cache.shift();
+        socket.emit(cacheItem[0], cacheItem[1]);
+    }
+});
+
 var nodejs = true;
+var position = [0,0]; // store gps location
 
 
 // allow the content of this page to be changed
@@ -40,13 +72,39 @@ socket.on('static', function (data) {
 
     // set title
     document.title = data.title + pid;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setLocation);
+    }
 });
+
+
+// send socket data, or cache
+function sendSocketData(message, data){
+    if(connected){
+        socket.emit(message, data);
+    }
+    else{
+        // cache data
+        var cacheItem = [message, data];
+        cache.push(cacheItem);
+    }
+}
 
 
 // send pid and guid to server, and which page we want to go to
 function start(destination){
     pid = document.getElementById('participantid').value;
     document.title += " " + pid;
-    var sid = document.getElementById('sessions').value;
+    sid = document.getElementById('sessions').value;
     socket.emit('start', { "pid": pid, "session": sid, "url": destination });
+}
+
+function updateLocation(){
+    navigator.geolocation.getCurrentPosition(setLocation);
+}
+
+function setLocation(pos) {
+    position = [pos.coords.latitude, pos.coords.longitude];
+    console.log(position);
 }
